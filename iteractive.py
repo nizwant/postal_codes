1aecb104b4ba035521286b2a7d6e16e018e3e3c4
nizwant <mateusznizwantowski@gmail.com>
2025-09-07 12:04:24 +0200

    feat: run on bigger sample size and validate it

--- iteractive.py
+++ iteractive.py
@@ -6,13 +6,15 @@ plt.ion()  # interactive mode ON
 
 tables = camelot.read_pdf(
     "oficjalny_spis_pna_2025.pdf",
-    flavor="stream",
+    flavor="hybrid",
     pages="3-22",
     table_areas=["28,813,567,27"],
+    columns=["60,144,267,332,422,497"],
+    row_tol=9,
+    process_background=True,
 )
-
-camelot.plot(tables[3], kind="contour")
-# fig = camelot.plot(tables[0], kind="textedge")
+camelot.plot(tables[1], kind="grid")
+# camelot.plot(tables[-2], kind="grid")
 plt.show()
 
 # Keep the plot open
