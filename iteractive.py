8ad6d75f679db77d81aa552e2e82138722a49550
nizwant <mateusznizwantowski@gmail.com>
2025-09-07 10:23:57 +0200

    feat: manual columns separators

--- iteractive.py
+++ iteractive.py
@@ -4,9 +4,15 @@ import matplotlib.pyplot as plt
 # Use interactive backend
 plt.ion()  # interactive mode ON
 
-tables = camelot.read_pdf("oficjalny_spis_pna_2025.pdf", flavor="stream", pages="3-22")
+tables = camelot.read_pdf(
+    "oficjalny_spis_pna_2025.pdf",
+    flavor="stream",
+    pages="3-22",
+    table_areas=["28,813,567,27"],
+)
 
-fig = camelot.plot(tables[0], kind="textedge")
+camelot.plot(tables[3], kind="contour")
+# fig = camelot.plot(tables[0], kind="textedge")
 plt.show()
 
 # Keep the plot open
